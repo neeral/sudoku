e5c50daabd6eb371e5c67c27dad2484aac81ea2f
Neeral Dodhia <neeral.dodhia@gmail.com>
2014-05-22 23:22:32 +0100

    Updated board.Counters class to prevent counts becoming negative.

--- src/board/Counters.java
+++ src/board/Counters.java
@@ -1,40 +1,43 @@
 package board;
 
-import java.util.Arrays;
-
 public class Counters {
 	// arrays to hold the count for how many cells in that row/column/square can take that value
 	// first index = value
 	// second index = row/column/square number 
-	private int[][] rows;
-	private int[][] columns;
-	private int[][][] squares;
+	private Counter[][] rows;
+	private Counter[][] columns;
+	private Counter[][][] squares;
 	
 	static final int MAX_CHOICES = 9;
 	static final int PENDING = 1;
 	static final int COMPLETE = 0;
 	
 	Counters() {
-		rows = new int[9][9];
-		for (int[] row : rows) Arrays.fill(row, 9);
+		rows = new Counter[9][9];
+		columns = new Counter[9][9];
+		for (int i=0; i<rows.length; i++)
+			for (int j=0; j<rows[i].length; j++) {
+				rows[i][j] = new Counter();
+				columns[i][j] = new Counter();
+			}
 		
-		columns = new int[9][9];
-		for (int[] col : columns) Arrays.fill(col, 9);
-		
-		squares = new int[3][3][9];
-		for (int[][] row : squares) for (int[] col : row) Arrays.fill(col, 9);
+		squares = new Counter[3][3][9];
+		for (int i=0; i<squares.length; i++)
+			for (int j=0; j<squares[i].length; j++)
+				for (int k=0; k<squares[i][j].length; k++)
+					squares[i][j][k] = new Counter();
 	}
 	
 	boolean eliminateChoice(int i, int value) {
 		final int x = BoardManager.getX(i);
 		final int y = BoardManager.getY(i);
 		
-		rows[y][value - 1]--;
-		columns[x][value - 1]--;
-		squares[x/3][y/3][value - 1]--;
+		rows[y][value - 1].decrement();
+		columns[x][value - 1].decrement();
+		squares[x/3][y/3][value - 1].decrement();
 		
 		// should only be "== 1" meaning only one cell can hold that value but I'm using "<=" to be safer
-		return rows[y][value - 1] <= PENDING || columns[x][value - 1] <= PENDING || squares[x/3][y/3][value - 1] <= PENDING;
+		return rows[y][value - 1].isPending() || columns[x][value - 1].isPending() || squares[x/3][y/3][value - 1].isPending();
 	}
 	
 	// only for use by construction phase
@@ -42,13 +45,13 @@ public class Counters {
 		final int x = BoardManager.getX(i);
 		final int y = BoardManager.getY(i);
 		
-		for (int j=0; j<rows[y].length; j++) rows[y][j]--;
-		for (int j=0; j<columns[x].length; j++) columns[x][j]--;
-		for (int j=0; j<squares[x/3][y/3].length; j++) squares[x/3][y/3][j]--;
+		for (int j=0; j<rows[y].length; j++) rows[y][j].decrement();
+		for (int j=0; j<columns[x].length; j++) columns[x][j].decrement();
+		for (int j=0; j<squares[x/3][y/3].length; j++) squares[x/3][y/3][j].decrement();
 		
-		rows[y][value - 1] = PENDING;
-		columns[x][value - 1] = PENDING;
-		squares[x/3][y/3][value - 1] = PENDING;
+		rows[y][value - 1].setPending();
+		columns[x][value - 1].setPending();
+		squares[x/3][y/3][value - 1].setPending();
 		// what about notifications?? could this be coded in a higher manager??
 	}
 	
@@ -56,9 +59,9 @@ public class Counters {
 		final int x = BoardManager.getX(i);
 		final int y = BoardManager.getY(i);
 		
-		rows[y][value - 1] = COMPLETE;
-		columns[x][value - 1] = COMPLETE;
-		squares[x/3][y/3][value - 1] = COMPLETE;
+		rows[y][value - 1].setComplete();
+		columns[x][value - 1].setComplete();
+		squares[x/3][y/3][value - 1].setComplete();
 	}
 	
 	private static class Counter {
@@ -70,8 +73,18 @@ public class Counters {
 			if (value > PENDING) value--;
 			return value;
 		}
+		boolean isPending(){
+			return value == PENDING;
+		}
+		void setPending() {
+			value = PENDING;
+		}
+		void setComplete() {
+			value = COMPLETE;
+		}
 	}
 
+/*
 	int getCountForRow(int i, int value) {
 		return rows[i][value - 1];		
 	}
@@ -83,6 +96,18 @@ public class Counters {
 	int getCountForSquare(int i, int j, int value) {
 		return squares[i][j][value - 1];
 	}
-	
+*/
+	boolean isPendingCountForRow(int i, int value) {
+		return rows[i][value - 1].isPending();	
+	}
+
+	boolean isPendingCountForColumn(int j, int value) {
+		return columns[j][value - 1].isPending();
+	}
+
+	boolean isPendingCountForSquare(int i, int j, int value) {
+		return squares[i][j][value - 1].isPending();
+	}
+
 	
 }
